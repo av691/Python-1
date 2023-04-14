d6388110a839b71735dad07eac6e0b0b5d4d6b23
Piotr Kruczek <piotr.kruczek@outlook.com>
2023-04-14 19:14:53 +0200

    2023-04-14

---
+++ P.01/3. Funkcja print i napisy (zadania).py
@@ -0,0 +1,14 @@
+# Zadanie nr 1
+# Zapisz jeszcze raz rozwiązanie zadania ze średnią prędkością biegu, tym razem jako skrypt.
+# Wypisz wynik wraz z informacją tekstową (np. średnia prędkość to...) wykorzystując funkcję print.
+time = 3
+distance = 38
+avg_speed = distance / time
+print("Średnia prędkość =", avg_speed)
+ 
+# Zadanie nr 2
+# Wypisz swoje ulubione potrawy, każdą z nich umieszczając w nowej linii.
+potrawa_1 = 'piwo'
+potrawa_2 = 'piwo'
+potrawa_3 = 'piwo'
+print("Moje ulubione potrawy to", potrawa_1, potrawa_2, potrawa_3, sep='\n')
