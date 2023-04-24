ce29a98ef7a36085154cafe022158af4a52bc05e
Piotr Kruczek <piotr.kruczek@outlook.com>
2023-04-24 18:37:24 +0200

    2023-04-24

---
+++ P.04/04. Instrukcja elif.py
@@ -0,0 +1,25 @@
+# Zadanie nr 1
+# Połącz kalkulator wartości lokaty i kredytowy.
+
+# Zapytaj użytkownika czy chce obliczyć wartość lokaty czy zdolność kredytową.
+
+# Następnie wyznacz oczekiwaną wartość.
+
+# Obsłuż (na przykład odpowiednim komunikatem) sytuację, w której użytkownik wybierze nieistniejącą opcję.
+
+
+
+# Zadanie nr 2
+# Zaimplementuj program interpretujący wynik uzyskany w teście Coopera.
+# Zapytaj o:
+
+# wiek
+# płeć
+# uzyskany wynik
+# Na tej podstawie zinterpretuj rezultat.
+
+# Możesz zaimplementować tylko wybrane przedziały wiekowe.
+
+age = input("Podaj wiek ")
+gender = input("Podaj płeć M/K ")
+result = input("Podaj uzyskany wynik ")
